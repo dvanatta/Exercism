b3cf3f1d81ba4a9a853c9628b94a2b0756af78be
Dan <Dan@Jenns-MacBook-Pro.local>
2014-09-23 15:03:33 -0700

    wrote bob

--- bob.py
+++ bob.py
@@ -1,1 +1,30 @@
 #This is bob
+# If all of the letters are caps its yelling
+# elif ends in question
+# elif blank
+# else whatever
+
+
+
+def check_yelling(string):
+    return string.isupper() 
+
+def check_question(string):
+    return string[len(string) - 1] == "?"
+
+def check_empty(string):
+    if len(string) == 0:
+	return True
+    else:
+	return string.isspace()
+
+def hey(string):
+    if check_empty(string):
+        return "Fine. Be that way!"
+    if check_yelling(string):
+	return "Whoa, chill out!"
+    if check_question(string):
+	return "Sure."
+    else:
+        return "Whatever."
+
